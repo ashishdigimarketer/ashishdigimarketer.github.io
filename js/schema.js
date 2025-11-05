f5b238b3ff42bd8ef93818989b30796cb086f91a
Ashish Kumar <ashishkgmid@gmail.com>
2025-11-05 21:19:26 +0530

    Create schema.js

---
+++ js/schema.js
@@ -0,0 +1,41 @@
+<script type="application/ld+json">
+		{
+		  "@context": "https://schema.org/",
+		  "@type": "Person",
+		  "name": "Ashish Kumar",
+           "alternateName": [
+      "Ashish",
+      "Ashish Digital Marketer",
+      "Ashish Professional Trader",
+      "Ashish Kumar Entrepreneur",
+      "Ashish Kumar Trader"
+    ],
+		  "url": "https://ashishdigimarketer.github.io/",
+		  "image": "https://ashishkumar.tech.blog/wp-content/uploads/2025/10/ashish-kumar.jpg",
+		  "jobTitle": [
+      "Digital Marketer",
+      "Professional Trader",
+      "Entrepreneur"
+    ]}
+		  "sameAs": [
+					"https://www.facebook.com/ashishkgmid/",
+					"https://x.com/ashishkgmid",
+					"https://www.instagram.com/ashishkgmid/",
+					"https://www.youtube.com/@ashishkgmid",
+					"https://www.linkedin.com/in/ashishkgmid/",
+					"https://www.pinterest.com/ashishkgmid/",
+					"https://github.com/ashishkgmid/",
+					"https://ashishdigimarketer.github.io/"
+					],
+			"email": "ashish@weblightinfotech.com",
+			"description": "Ashish Kumar is a digital marketer with over 8 years of experience in digital marketing.",
+"homeLocation": {
+			"@type": "Place",
+			"name": "Delhi"
+				},
+"affiliation": {
+			"@type": "Organization",
+			"name": "Web Light Infotech"
+			  }
+			}
+</script>
