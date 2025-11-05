b90a8511382615eb011e56461ef1c8b93447e46a
Ashish Kumar <ashishkgmid@gmail.com>
2025-11-05 22:51:53 +0530

    Update schema.js

--- js/schema.js
+++ js/schema.js
@@ -1,4 +1,35 @@
 <script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  "name": "Ashish Kumar",
+  "url": "https://ashishdigimarketer.github.io/",
+  "image": "https://ashishkumar.tech.blog/wp-content/uploads/2025/10/ashish-kumar.jpg",
+  "sameAs": [
+   "https://www.facebook.com/ashishkgmid/",
+		"https://twitter.com/ashishkgmid",
+		"https://x.com/ashishkgmid",
+		"https://www.instagram.com/ashishkgmid/",
+		"https://www.youtube.com/@ashishkgmid",
+		"https://www.linkedin.com/in/ashishkgmid/",
+		"https://www.pinterest.com/ashishkgmid/",
+		"https://github.com/ashishkgmid/",
+		"https://orcid.org/0009-0004-4401-3695",
+		"https://gravatar.com/ashishkgmid",
+		"https://www.flickr.com/photos/ashishkgmid/",
+		"https://www.flickr.com/people/ashishkgmid/",
+		"https://scholar.google.com/citations?user=00IhU2wAAAAJ&hl=en",
+		"https://ashishdigimarketer.github.io/",
+		"https://www.google.com/search?kgmid=/g/11yn9v8qp6"
+  ],
+  "jobTitle": "Digital Marketer",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Web Light Infotech"
+  }  
+}
+</script>
+<script type="application/ld+json">
 		{
 		  "@context": "https://schema.org/",
 		  "@type": "Person",
@@ -10,23 +41,39 @@
       "Ashish Kumar Entrepreneur",
       "Ashish Kumar Trader"
     ],
-		  "url": "https://ashishdigimarketer.github.io/",
-		  "image": "https://ashishkumar.tech.blog/wp-content/uploads/2025/10/ashish-kumar.jpg",
-		  "jobTitle": [
+    "image": [
+      {
+        "@type": "ImageObject",
+        "url": "https://ashishkumar.tech.blog/wp-content/uploads/2025/10/ashish-kumar.jpg",
+        "contentUrl": "https://ashishkumar.tech.blog/wp-content/uploads/2025/10/ashish-kumar.jpg",
+        "caption": "Ashish Kumar"
+      },
+      {
+        "@type": "ImageObject",
+        "url": "https://commons.wikimedia.org/wiki/File:Ashish_Kumar.jpg",
+        "contentUrl": "https://commons.wikimedia.org/wiki/File:Ashish_Kumar.jpg",
+        "caption": "Ashish Kumar"
+      }
+      ],
+	"jobTitle": [
       "Digital Marketer",
       "Professional Trader",
       "Entrepreneur"
-    ]}
-		  "sameAs": [
-					"https://www.facebook.com/ashishkgmid/",
-					"https://x.com/ashishkgmid",
-					"https://www.instagram.com/ashishkgmid/",
-					"https://www.youtube.com/@ashishkgmid",
-					"https://www.linkedin.com/in/ashishkgmid/",
-					"https://www.pinterest.com/ashishkgmid/",
-					"https://github.com/ashishkgmid/",
-					"https://ashishdigimarketer.github.io/"
-					],
+    ],
+	
+            "identifier": [
+      {
+        "@type": "PropertyValue",
+        "name": "kgmid",
+        "value": "/g/11yn9v8qp6",
+        "url": "https://www.google.com/search?kgmid=/g/11yn9v8qp6"
+      },
+      {
+        "@type": "PropertyValue",
+        "name": "orcid",
+        "value": "0009-0004-4401-3695",
+        "url": "https://orcid.org/0009-0004-4401-3695"
+      }],
 			"email": "ashish@weblightinfotech.com",
 			"description": "Ashish Kumar is a digital marketer with over 8 years of experience in digital marketing.",
 "homeLocation": {
@@ -37,5 +84,5 @@
 			"@type": "Organization",
 			"name": "Web Light Infotech"
 			  }
-			}
+			}}
 </script>
