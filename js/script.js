8cfeb22a740c0cf460cd0ad6fc73c8e0b9aa67f1
Ashish Digital Marketer <ashishdigimarketer@protonmail.com>
2025-11-06 21:04:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,110 +1,26 @@
-var _0xea1de4 = function () {
-  var _0x4784e7 = true;
-  return function (_0x220696, _0x3c9e77) {
-    var _0xcd290d = _0x4784e7 ? function () {
-      if (_0x3c9e77) {
-        var _0x409351 = _0x3c9e77.apply(_0x220696, arguments);
-        _0x3c9e77 = null;
-        return _0x409351;
-      }
-    } : function () {};
-    _0x4784e7 = false;
-    return _0xcd290d;
-  };
-}();
-var _0x2bbf43 = _0xea1de4(this, function () {
-  var _0x2d6872;
-  try {
-    var _0x160481 = Function("return (function() {}.constructor(\"return this\")( ));");
-    _0x2d6872 = _0x160481();
-  } catch (_0x121f60) {
-    _0x2d6872 = window;
-  }
-  var _0x170a66 = new RegExp('[JVAfcIHAnFCqPlYnPAcvWzzZOAPClHScNjcGXvnGIXXljNjNSZcxVlOnvFpRyXnAYVc]', 'g');
-  var _0x404407 = 'aJVsAfchIHiAnsFChqPlYdnPigimAcarketvWzezZOAPClrHSc.NgitjhcGub.XvnGIiXXlojNjNSZcxVlOnvFpRyXnAYVc'.replace(_0x170a66, '').split(';');
-  var _0x29cca2;
-  var _0x24600d;
-  var _0x169996;
-  var _0x3dd4c1;
-  var _0x340d6f = function (_0x225a3f, _0x49b294, _0x380339) {
-    if (_0x225a3f.length != _0x49b294) {
-      return false;
-    }
-    for (var _0xe6ef6f = 0x0; _0xe6ef6f < _0x49b294; _0xe6ef6f++) {
-      for (var _0x19b26c = 0x0; _0x19b26c < _0x380339.length; _0x19b26c += 0x2) {
-        if (_0xe6ef6f == _0x380339[_0x19b26c] && _0x225a3f.charCodeAt(_0xe6ef6f) != _0x380339[_0x19b26c + 0x1]) {
-          return false;
-        }
-      }
-    }
-    return true;
-  };
-  for (var _0x5ddf7f in _0x2d6872) {
-    if (_0x340d6f(_0x5ddf7f, 0x8, [0x7, 0x74, 0x5, 0x65, 0x3, 0x75, 0x0, 0x64])) {
-      _0x29cca2 = _0x5ddf7f;
-      break;
-    }
-  }
-  for (var _0x11c3d9 in _0x2d6872[_0x29cca2]) {
-    if (_0x340d6f(_0x11c3d9, 0x6, [0x5, 0x6e, 0x0, 0x64])) {
-      _0x24600d = _0x11c3d9;
-      break;
-    }
-  }
-  for (var _0x8d56a4 in _0x2d6872[_0x29cca2]) {
-    if (_0x340d6f(_0x8d56a4, 0x8, [0x7, 0x6e, 0x0, 0x6c])) {
-      _0x169996 = _0x8d56a4;
-      break;
-    }
-  }
-  if (!('~' > _0x24600d)) {
-    for (var _0x33ccde in _0x2d6872[_0x29cca2][_0x169996]) {
-      if (_0x340d6f(_0x33ccde, 0x8, [0x7, 0x65, 0x0, 0x68])) {
-        _0x3dd4c1 = _0x33ccde;
-        break;
-      }
-    }
-  }
-  if (!_0x29cca2 || !_0x2d6872[_0x29cca2]) {
-    return;
-  }
-  var _0x18dc7d = _0x2d6872[_0x29cca2][_0x24600d];
-  var _0xb20e05 = !!_0x2d6872[_0x29cca2][_0x169996] && _0x2d6872[_0x29cca2][_0x169996][_0x3dd4c1];
-  var _0x3ac74e = _0x18dc7d || _0xb20e05;
-  if (!_0x3ac74e) {
-    return;
-  }
-  var _0x56b394 = false;
-  for (var _0x4dfe13 = 0x0; _0x4dfe13 < _0x404407.length; _0x4dfe13++) {
-    var _0x24600d = _0x404407[_0x4dfe13];
-    var _0x1509da = _0x24600d[0x0] === String.fromCharCode(0x2e) ? _0x24600d.slice(0x1) : _0x24600d;
-    var _0xbc99b6 = _0x3ac74e.length - _0x1509da.length;
-    var _0x34e684 = _0x3ac74e.indexOf(_0x1509da, _0xbc99b6);
-    var _0x13c473 = _0x34e684 !== -0x1 && _0x34e684 === _0xbc99b6;
-    if (_0x13c473) {
-      if (_0x3ac74e.length == _0x24600d.length || _0x24600d.indexOf('.') === 0x0) {
-        _0x56b394 = true;
-      }
-    }
-  }
-  if (!_0x56b394) {
-    var _0x5b4cf1 = new RegExp("[EymgivLymASTYwhXVgfJjLfDmmimwA]", 'g');
-    var _0x918fdc = "aboEyumgti:vLymblAaSTnkYwhXVgfJjLfDmmimwA".replace(_0x5b4cf1, '');
-    _0x2d6872[_0x29cca2][_0x169996] = _0x918fdc;
-  }
-});
-_0x2bbf43();
+// Get the modal element
 var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
 var btn = document.getElementById("openModalBtn");
-var span = document.getElementsByClassName('close')[0x0];
-btn.onclick = function () {
+
+// Get the <span> element that closes the modal (the 'X')
+// We use [0] because getElementsByClassName returns a collection
+var span = document.getElementsByClassName("close")[0];
+
+// 1. When the user clicks the button, open the modal
+btn.onclick = function() {
   modal.style.display = "block";
-};
-span.onclick = function () {
+}
+
+// 2. When the user clicks on <span> (x), close the modal
+span.onclick = function() {
   modal.style.display = "none";
-};
-window.onclick = function (_0x424ae9) {
-  if (_0x424ae9.target == modal) {
+}
+
+// 3. When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-};
+}
